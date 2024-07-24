ad6da2bf3cdd9d9365aff889f400c2574beab638
ocahs9 <piorag99@gmail.com>
2024-07-24 20:30:03 +0900

    feat: Login 첫 화면 UI 퍼블리싱

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,18 @@
+import kakaoButtonImg from "@assets/images/kakao_login_medium_narrow.png"
 import * as S from "./Login.styled"
 
 const Login = () => {
   return (
-    <S.text>
-      테스트입니다!
-    </S.text>
+    <S.LoginWrapper>
+      <S.LoginTitle>오늘 작업도 파이팅!</S.LoginTitle>
+      <S.LoginTitleExplanation>
+        로그인 시 작업 시간 기록과 분석 리포트 등 
+        다양한 기능을 사용할 수 있어요
+      </S.LoginTitleExplanation>
+      <S.LoginGraphic>아직 내용 없음</S.LoginGraphic>
+      <S.LoginSubTitle>나의 야작 메이트, LOGO, 슬로건</S.LoginSubTitle>
+      <S.LoginButton $imgsrc = {kakaoButtonImg}/>
+    </S.LoginWrapper>
   )
 }
 
--- src/pages/Login/Login.styled.js
+++ src/pages/Login/Login.styled.js
@@ -1,7 +1,49 @@
 import styled from "styled-components";
 
-export const text = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: ${({ theme }) => theme.colors.Main_Red};
+export const LoginWrapper = styled.div`
+  width: 37.5rem;
+  min-height: 66.7rem;//75.6rem; -> 844 - 88
+  background-color: ${({ theme }) => theme.colors.Background_Black};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginTitle = styled.span`
+  text-align: center;
+  color: ${({ theme }) => theme.colors.White};
+  ${({ theme }) => theme.fonts.Header_2};
+`
+
+export const LoginTitleExplanation = styled.span`
+  text-align: center;
+  color: ${({ theme }) => theme.colors.White};
+  ${({ theme }) => theme.fonts.Body_2_2};
+
+  width: 23.4rem;
+  margin-top: 12px;
+`
+
+export const LoginGraphic = styled.div`
+  width: 26.5rem;
+  height: 32.9rem;
+  background-color: aliceblue;
+
+  margin-top: 28px;
+`
+
+export const LoginSubTitle = styled.div`
+  color: ${({ theme }) => theme.colors.White};
+  ${({ theme }) => theme.fonts.Subhead_2_2};
+  margin-top: 16px;
+`
+
+export const LoginButton = styled.button`
+  width: 18.3rem;
+  height: 4.5rem;
+  background-image: url(${(props) => props.$imgsrc});
+
+  margin-top: 32px;
 `
