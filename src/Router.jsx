9f2318d3827b99adb6c2b9d451fa047c0e7f7284
ocahs9 <piorag99@gmail.com>
2024-07-24 19:26:38 +0900

    vitejs/plugin-react 라이브러리 설치

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
+import Login from "@pages/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login/Login";
 // 예시
 //import Search from "./pages/Search/Search";
 //import Main from "./pages/Main/Main";
