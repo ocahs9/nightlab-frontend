12ba9f67d8fa654cd1afd9fcedb604b2a34c4c4f
ocahs9 <piorag99@gmail.com>
2024-08-02 00:22:29 +0900

    feat: 비로그인 페이지까지 구성 완료

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import KakaoLogin from "@pages/KakaoLogin/KakaoLogin";
 import Login from "@pages/Login/Login";
 import LoginSchool from "@pages/LoginSchool/LoginSchool";
 import ProfileSetup from "@pages/ProfileSetup/ProfileSetup";
+import GuestPage from "@pages/Timer/components/GuestPage/GuestPage";
+import Timer from "@pages/Timer/Timer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // 예시
 //import Search from "./pages/Search/Search";
@@ -16,6 +18,8 @@ const Router = () => {
         <Route path="/school" element={<LoginSchool/>}/>
         <Route path="/profile-setup" element={<ProfileSetup/>}/>,
         <Route path="/oauth/callback/kakao" element={<KakaoLogin/>} />,
+        <Route path="/unlogin" element={<GuestPage/>}/>,
+        <Route path="/timer" element={<Timer/>}/>,
         {/* 
         <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
--- src/pages/Timer/components/GuestPage/GuestPage.jsx
+++ src/pages/Timer/components/GuestPage/GuestPage.jsx
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import * as S from "./GuestPage.styled";
 
 const GuestPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <S.GuestPageWrapper>
+      <S.BodyWrapper>
+        <S.TextWrapper>
+          <S.Title>비로그인 상태예요</S.Title>
+          <S.SubTitle>
+            {//여러 줄일 경우 반드시 백틱 사용할 것 - 그럼 \n 안써도 개행 인식 가능(물론 pre-line은 설정해야함)
+            `일회적인 타이머 사용만 가능해요
+            시간 기록과 목표 작성 및 상태메시지 기록은 불가능해요`}
+          </S.SubTitle>
+        </S.TextWrapper>
+        <S.GraphicImg/>
+        <S.ButtonWrapper>
+          <S.Button onClick={()=>navigate("/")}>로그인하기</S.Button>
+          <S.Button onClick={()=>navigate("/timer")}>무시하고 타이머 시작</S.Button>
+        </S.ButtonWrapper>
+      </S.BodyWrapper>
       
-    </div>
+    </S.GuestPageWrapper>
   )
 }
 
