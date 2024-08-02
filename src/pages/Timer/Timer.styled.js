05596c8aac197d0ec616009289102c01dabb8a20
ocahs9 <piorag99@gmail.com>
2024-08-02 20:25:51 +0900

    fix: 버튼 비활성화시, hover에도 변하지 않도록 구현

--- src/pages/Timer/Timer.styled.js
+++ src/pages/Timer/Timer.styled.js
@@ -85,16 +85,20 @@ export const TimerButton = styled.button`
   border: 1px solid var(--Grayscale-White, #FFF);
   background: rgba(255, 255, 255, 0.25);
   
-  &:hover{
-    color: var(--main-red-red-50-main, #FF7F7F);
-    border: 1px solid var(--main-red-red-50-main, #FF7F7F);
-    background: rgba(255, 127, 127, 0.25);
-  }
+  ${({ $disabled }) => !$disabled && css`
+    &:hover{
+      color: var(--main-red-red-50-main, #FF7F7F);
+      border: 1px solid var(--main-red-red-50-main, #FF7F7F);
+      background: rgba(255, 127, 127, 0.25);
+    }
+  `};
+  
 
   ${({ $disabled }) => $disabled && css`
     color: rgba(237, 241, 245, 0.25);
     border: 1px solid rgba(237, 241, 245, 0.25);
     background: rgba(237, 241, 245, 0.25);
+    cursor: not-allowed;
   `};
 
   ${({ $stopText }) => $stopText && css`
