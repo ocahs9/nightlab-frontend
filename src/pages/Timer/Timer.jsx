fd2efa05928e07179fc8eb799f2840684a68e6f8
ocahs9 <piorag99@gmail.com>
2024-08-02 20:19:59 +0900

    feat: 타이머 부분 뷰 구현

--- src/pages/Timer/Timer.jsx
+++ src/pages/Timer/Timer.jsx
@@ -1,9 +1,24 @@
+import * as S from "./Timer.styled"
 
 const Timer = () => {
   return (
-    <div>
-      타이머 페이지
-    </div>
+    <S.TimerPageWrapper>
+      <S.TimerWrapper>
+        <S.TimerTitle>작업 타이머</S.TimerTitle>
+        <S.MyStatusWrapper>
+          <S.MyStatusTimer>01:24:10째 작업 중</S.MyStatusTimer>
+          <S.MyStatusGraphic $profile={`gentle`} />
+        </S.MyStatusWrapper>
+        <S.TimerButtonWrapper>
+          <S.TimerButton>작업 타이머 시작</S.TimerButton>
+          <S.TimerButton disabled={true} $disabled={true}>휴식하기</S.TimerButton>
+          <S.TimerSubTitle>휴식 버튼으로 작업 타이머를 중지하지 않고 휴식만 할 수 있어요</S.TimerSubTitle>
+        </S.TimerButtonWrapper>
+      </S.TimerWrapper>
+      <S.OnlineMemberWrapper>
+
+      </S.OnlineMemberWrapper>
+    </S.TimerPageWrapper>
   )
 }
 
--- src/pages/Timer/Timer.styled.js
+++ src/pages/Timer/Timer.styled.js
@@ -1,4 +1,8 @@
-import styled from "styled-components";
+import gentle from "@assets/svgs/gentle_timer.svg";
+import serious from "@assets/svgs/serious_timer.svg";
+import sharp from "@assets/svgs/sharp_timer.svg";
+import styled, { css } from "styled-components";
+
 
 export const TimerPageWrapper = styled.section`
   display: flex;
@@ -14,11 +18,18 @@ export const TimerPageWrapper = styled.section`
 export const TimerWrapper = styled.article`
   height: 646px;
   align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export const TimerTitle = styled.span`
   color: ${({ theme }) => theme.colors.White};
   ${({ theme }) => theme.fonts.Header_3};
+  align-self: flex-start;
+  margin-left: 16px;
+  margin-bottom: 24px;
 `
 
 export const MyStatusWrapper = styled.div`
@@ -34,11 +45,70 @@ export const MyStatusTimer = styled.span`
   color: ${({ theme }) => theme.colors.White};
   ${({ theme }) => theme.fonts.Header_4_1};
 `
+
+// 프로필 이름을 SVG 경로와 매핑하는 객체
+const profileImages = {
+  gentle,
+  serious,
+  sharp,
+};
+
+//gentle, sharp, serious
 export const MyStatusGraphic = styled.div`
   height: 337px;
   align-self: stretch;
+  background-image: ${({ $profile }) => `url(${profileImages[$profile]})`};
+`
+export const TimerButtonWrapper = styled.div`
+  display: flex;
+  width: 358px;
+  min-height: 128px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  flex-shrink: 0;
 `
 
+export const TimerButton = styled.button`
+  display: flex;
+  width: 358px;
+  height: 56px;
+  padding: 16px 96px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+
+  border-radius: 28px;
+  color: var(--Grayscale-White, #FFF);
+  border: 1px solid var(--Grayscale-White, #FFF);
+  background: rgba(255, 255, 255, 0.25);
+  
+  &:hover{
+    color: var(--main-red-red-50-main, #FF7F7F);
+    border: 1px solid var(--main-red-red-50-main, #FF7F7F);
+    background: rgba(255, 127, 127, 0.25);
+  }
+
+  ${({ $disabled }) => $disabled && css`
+    color: rgba(237, 241, 245, 0.25);
+    border: 1px solid rgba(237, 241, 245, 0.25);
+    background: rgba(237, 241, 245, 0.25);
+  `};
+
+  ${({ $stopText }) => $stopText && css`
+    color: var(--main-red-red-50-main, #FF7F7F);
+    border: 1px solid var(--main-red-red-50-main, #FF7F7F);
+    background: rgba(255, 127, 127, 0.25);
+  `};
+`
+export const TimerSubTitle = styled.div`
+  width: 358px;
+  color: ${({ theme }) => theme.colors.Gray_50};
+  text-align: center;
+  ${({ theme }) => theme.fonts.Caption_1_2};
+`
 
 /*
   온라인 멤버 부분
