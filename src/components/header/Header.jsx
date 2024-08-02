40c6c892be4d696f5568c1be3b57736ff7a27b2f
DefineX <keaikim21@naver.com>
2024-08-02 18:58:10 +0900

    Header.jsx 최신화

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,118 @@
-import styled from "styled-components";
-import theme from "../../styles/theme";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useData } from "../../contexts/WholeContext";
 
-const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
+import * as H from "./Header.styled";
 
-const HeaderOptions = styled.div`
-  display: flex;
+import mainLogo from "../../assets/mainLogo.svg";
+import closeIcon from "../../assets/svgs/closeIcon.svg";
 
-  .login-btn {
-    margin: 0;
-    padding: 2px 14px;
-    width: 65px;
-    height: 24px;
-    color: #ffffff;
-    border: 1px solid #FFFFFF;
-    border-radius: 18px;
-  }
-`;
+const Header = ({ toggleMenu }) => {
+  const { isAuthenticated, login, logout, header } = useData();
 
-function Header() {
+  const navigate = useNavigate();
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+    toggleMenu();
+  };
+
+  // console.log("isAuthenticated", isAuthenticated);
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
   return (
-    <>
-      <HeaderContainer>
-        <h2>Night Lab</h2>
-        <HeaderOptions>
-          <h2 className="login-btn subhead4-1">로그인</h2>
-          <a>
-            <span></span>
-          </a>
-        </HeaderOptions>
-      </HeaderContainer>
-    </>
+    <H.Container>
+      <H.NavBar>
+        {header.showLogo && (
+          <Link to="/">
+            <img src={mainLogo} alt="MainLogo" className="mainLogo" />
+          </Link>
+        )}
+        {isAuthenticated ? (
+          <H.Options>
+            {header.showLoginButton && (
+              <Link to="/">
+                <button className="login-btn">로그아웃</button>
+              </Link>
+            )}
+            {header.showHamburgerButton && (
+              <>
+                <input
+                  type="checkbox"
+                  id="menu-toggle"
+                  onChange={handleMenuToggle}
+                  checked={menuOpen}
+                />
+                <H.HamburgerButton htmlFor="menu-toggle" id="menu-button">
+                  <span></span>
+                </H.HamburgerButton>
+              </>
+            )}
+          </H.Options>
+        ) : (
+          <H.Options>
+            {header.showLoginButton && (
+              <Link to="/login">
+                <button className="login-btn">로그인</button>
+              </Link>
+            )}
+            {header.showHamburgerButton && (
+              <>
+                <input
+                  type="checkbox"
+                  id="menu-toggle"
+                  onChange={handleMenuToggle}
+                  checked={menuOpen}
+                />
+                <H.HamburgerButton htmlFor="menu-toggle" id="menu-button">
+                  <span></span>
+                </H.HamburgerButton>
+              </>
+            )}
+            {header.showCloseButton && (
+              <>
+                <img
+                  src={closeIcon}
+                  alt="closeIcon"
+                  className="close-btn"
+                  onClick={handleHomeClick}
+                />
+              </>
+            )}
+          </H.Options>
+        )}
+      </H.NavBar>
+
+      {menuOpen && (
+        <H.MenuItems
+          style={{
+            transform: menuOpen ? "translateX(0%)" : "translateX(150%)",
+          }}
+        >
+          <li>실시간 유저 수 보기</li>
+
+          <Link to="/timer">
+            <li>작업 타이머</li>
+          </Link>
+
+          <Link to="/report">
+            <button disabled={!isAuthenticated}>작업 패턴 분석 리포트</button>
+          </Link>
+
+          <Link to="/mypage">
+            <button className="last-menu" disabled={!isAuthenticated}>
+              마이페이지
+            </button>
+          </Link>
+        </H.MenuItems>
+      )}
+    </H.Container>
   );
-}
+};
 
 export default Header;
