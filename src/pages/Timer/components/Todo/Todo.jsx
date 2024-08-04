4ac2fcd0692d61cda42da98576cc427b453042bb
ocahs9 <piorag99@gmail.com>
2024-08-04 17:59:55 +0900

    fix: 객체를 순환하며 렌더링하는 로직으로 수정

--- src/pages/Timer/components/Todo/Todo.jsx
+++ src/pages/Timer/components/Todo/Todo.jsx
@@ -1,8 +1,17 @@
 import { IcCheckGoal, IcPlusRed, IcUncheckGoal, IcXRed } from "@assets/svgs/index";
+import { useState } from "react";
 import * as S from "./Todo.styled";
 
 const Todo = () => {
-  const test = true;
+  const [isTodoOpen, setIsTodoOpen] = useState(false);
+  const [todos, setTodos] = useState({
+    1: {text: '0721 목표작업', completed: false, isEditing: false },
+    2: {text: '0721 목표작업', completed: false, isEditing: false },
+    3: {text: '0721 목표작업', completed: false, isEditing: false },
+    4: {text: '0721 목표작업', completed: false, isEditing: false },
+    5: {text: '0721 목표작업', completed: false, isEditing: false },
+  })
+  
 
   return (
     <S.TodoWrapper>
@@ -16,61 +25,20 @@ const Todo = () => {
             <S.TodoListTitle>목표 작업<IcPlusRed/></S.TodoListTitle>
             <S.TodoListCloseBtn><IcXRed/></S.TodoListCloseBtn>
           </S.TodoListHeader>
+
           <S.TodoListContainer>
-            <S.TodoListBox>
+            {Object.entries(todos).map(([id,todo])=>(
+              <S.TodoListBox key={id}>
               <S.TodoListBoxContent>
-                {test? <IcCheckGoal/> : <IcUncheckGoal/>}
-                <S.TodoListBoxText>0721 목표작업</S.TodoListBoxText>
+                {todo.completed? <IcCheckGoal/> : <IcUncheckGoal/>}
+                <S.TodoListBoxText>{todo.text}</S.TodoListBoxText>
               </S.TodoListBoxContent>
               <S.TodoListBoxEdit>
                 <S.TodoListBoxEditBtn/>
                 <S.TodoListBoxDeleteBtn/>
               </S.TodoListBoxEdit>
             </S.TodoListBox>
-
-            <S.TodoListBox>
-              <S.TodoListBoxContent>
-                {test? <IcCheckGoal/> : <IcUncheckGoal/>}
-                <S.TodoListBoxText>0721 목표작업</S.TodoListBoxText>
-              </S.TodoListBoxContent>
-              <S.TodoListBoxEdit>
-                <S.TodoListBoxEditBtn/>
-                <S.TodoListBoxDeleteBtn/>
-              </S.TodoListBoxEdit>
-            </S.TodoListBox>
-
-            <S.TodoListBox>
-              <S.TodoListBoxContent>
-                {test? <IcCheckGoal/> : <IcUncheckGoal/>}
-                <S.TodoListBoxText>0721 목표작업</S.TodoListBoxText>
-              </S.TodoListBoxContent>
-              <S.TodoListBoxEdit>
-                <S.TodoListBoxEditBtn/>
-                <S.TodoListBoxDeleteBtn/>
-              </S.TodoListBoxEdit>
-            </S.TodoListBox>
-
-            <S.TodoListBox>
-              <S.TodoListBoxContent>
-                {test? <IcCheckGoal/> : <IcUncheckGoal/>}
-                <S.TodoListBoxText>0721 목표작업</S.TodoListBoxText>
-              </S.TodoListBoxContent>
-              <S.TodoListBoxEdit>
-                <S.TodoListBoxEditBtn/>
-                <S.TodoListBoxDeleteBtn/>
-              </S.TodoListBoxEdit>
-            </S.TodoListBox>
-
-            <S.TodoListBox>
-              <S.TodoListBoxContent>
-                {test? <IcCheckGoal/> : <IcUncheckGoal/>}
-                <S.TodoListBoxText>0721 목표작업</S.TodoListBoxText>
-              </S.TodoListBoxContent>
-              <S.TodoListBoxEdit>
-                <S.TodoListBoxEditBtn/>
-                <S.TodoListBoxDeleteBtn/>
-              </S.TodoListBoxEdit>
-            </S.TodoListBox>
+            ))}
           </S.TodoListContainer>
 
         </S.TodoListLayout>
