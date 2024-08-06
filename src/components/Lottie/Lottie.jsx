8d81cf63967439d85c7da7afa14f312364787272
DefineX <keaikim21@naver.com>
2024-08-06 20:43:47 +0900

    Lottie JSON 파일 라이브러리 react-lottie -> lottie-react로 변경 및 타이머 그래픽 렌더링 완료

--- src/components/Lottie/Lottie.jsx
+++ src/components/Lottie/Lottie.jsx
@@ -1,53 +1,41 @@
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 
 import lightHouse from "../../animations/main_final.json";
 import timerGraphic from "../../animations/timer_final.json";
 
 export const LightHouseGraphic = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: lightHouse,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
-    <>
-      <div
-        style={{
-          width: "264px",
-          height: "434px",
-        }}
-      >
-        <Lottie options={defaultOptions} />
-      </div>
-    </>
+    <div
+      style={{
+        width: "264px",
+        height: "434px",
+      }}
+    >
+      <Lottie
+        animationData={lightHouse}
+        loop={true}
+        autoplay={true}
+        style={{ height: "100%", width: "100%" }}
+      />
+    </div>
   );
 };
 
 export const EnterTimerGraphic = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: timerGraphic,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
-    <>
-      <div
-        style={{
-          width: "360px",
-          height: "317px",
-          margin: "16px auto 24px",
-        }}
-      >
-        <Lottie options={defaultOptions} />
-      </div>
-    </>
+    <div
+      style={{
+        width: "360px",
+        height: "317px",
+        margin: "16px auto 24px",
+      }}
+    >
+      <Lottie
+        animationData={timerGraphic}
+        loop={true}
+        autoplay={true}
+        style={{ height: "100%", width: "100%" }}
+      />
+    </div>
   );
 };
