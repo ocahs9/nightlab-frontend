1b192100025b0085d92c6993ea5999bfa0e78363
ocahs9 <piorag99@gmail.com>
2024-08-06 20:36:41 +0900

    fix: 두번 렌더링 수정, openid 사용하도록 수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App.jsx";
+import { ContextProvider } from "./contexts/WholeContext.jsx";
 import Router from "./Router.jsx";
 import GlobalStyle from "./styles/global.js";
 import theme from "./styles/theme.js";
-import { ContextProvider } from "./contexts/WholeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <ContextProvider>
         <GlobalStyle />
@@ -16,5 +15,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
       </ContextProvider>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
